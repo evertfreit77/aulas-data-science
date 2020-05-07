e0bfeeca4c021edb3a531cb1583f7a453b2dadfd
Everton Fr <evertonfreitas59@hotmail.com>
2020-05-06 21:10:31 -0300

    Adicionando o resto dos comandos da aula 01

--- aula0.py
+++ aula0.py
@@ -9,3 +9,16 @@ notas['nota']
 notas['nota'].value_counts()
 
 notas['nota'].mean()
+
+notas.nota.plot()
+
+notas.nota.plot(kind='hist')
+
+print("Média",notas['nota'].mean())
+print("Mediana",notas['nota'].median())
+
+notas.nota.describe()
+
+import seaborn as sns
+
+sns.boxplot(notas.nota)
